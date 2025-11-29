1a92acb5f76b7642135ae06e6bb05b86ac828e50
linawsm52 <lenaexo0@gmail.com>
2025-11-29 20:22:28 +0000

    new file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 a = 1
 b = 2
-
 add = __import__("add_0").add
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
