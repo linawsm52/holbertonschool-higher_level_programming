ba5831d3341d67076b0bf3540d1d40043d12939b
root <lenaexo0@gmail.com>
2025-12-06 19:27:13 +0000

    new file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
 
     roman_map = {
