675567b9eb24ac03307714bd272ab77672ac15e9
linawsm52 <lenaexo0@gmail.com>
2025-12-16 13:47:16 +0000

    dd

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,8 +2,14 @@
 """Prints a formatted name."""
 
 
-def say_my_name(first_name, last_name=""):
+def say_my_name(first_name=None, last_name=""):
     """Prints My name is <first_name> <last_name>."""
+    if first_name is None:
+        raise TypeError(
+            "say_my_name() missing 2 required positional arguments: "
+            "'first_name' and 'last_name'"
+        )
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
