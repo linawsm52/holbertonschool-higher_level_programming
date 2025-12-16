b18d54d3f70af399cf8bae81ba07633094b0ae8f
linawsm52 <lenaexo0@gmail.com>
2025-12-16 13:28:42 +0000

    dd

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,14 +6,22 @@ def add_integer(a, b=98):
 
     a and b must be integers or floats, otherwise raise a TypeError.
     """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
+    # NaN check
     if a != a:
         raise TypeError("a must be an integer")
     if b != b:
+        raise TypeError("b must be an integer")
+
+    # Infinity / overflow check
+    if a == float("inf") or a == float("-inf"):
+        raise TypeError("a must be an integer")
+    if b == float("inf") or b == float("-inf"):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
