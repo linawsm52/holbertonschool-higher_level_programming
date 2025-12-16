9b3e5cfcf3eacc2ae64e8254a90e47614967a20c
linawsm52 <lenaexo0@gmail.com>
2025-12-16 09:46:13 +0000

    new file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""
-Module that contains add_integer function
-"""
+"""Defines a function that adds 2 integers."""
 
 
 def add_integer(a, b=98):
-    """
-    Adds two integers.
-
-    a and b must be integers or floats.
-    If they are floats, they are casted to integers.
-    """
+    """Adds two integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-
     if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # رفض NaN و inf و -inf (بدون math)
+    if isinstance(a, float) and (a != a or a in (float('inf'), float('-inf'))):
+        raise TypeError("a must be an integer")
+    if isinstance(b, float) and (b != b or b in (float('inf'), float('-inf'))):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
