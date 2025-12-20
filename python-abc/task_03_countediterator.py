82873f876dceb575f9b02389e7bf4ce5b464c35d
linawsm52 <lenaexo0@gmail.com>
2025-12-20 20:06:04 +0000

    ff

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""CountedIterator - keeps track of how many items were iterated."""
 
+class CountedIterator:
+    """Iterator wrapper that counts how many items have been returned."""
 
-class CounterIterator:
     def __init__(self, iterable):
         self.iterator = iter(iterable)
         self.count = 0
@@ -10,7 +12,7 @@ class CounterIterator:
         return self
 
     def __next__(self):
-        item = next(self.iterator)
+        item = next(self.iterator)  # raises StopIteration automatically
         self.count += 1
         return item
 
