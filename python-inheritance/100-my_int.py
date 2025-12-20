b08bcaf6f58b80fde2d3edc44fe3bcf693719a8f
linawsm52 <lenaexo0@gmail.com>
2025-12-20 19:52:20 +0000

    new file

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+This module defines a custom integer class MyInt
+with inverted equality operators.
+"""
 
 
 class MyInt(int):
+    """
+    MyInt is a rebel class that inverts == and != operators.
+    """
+
     def __eq__(self, other):
         return super().__ne__(other)
 
