c638a014c0a1dc0601ae248cac5e43ea346ededb
linawsm52 <lenaexo0@gmail.com>
2025-12-20 17:46:47 +0000

    new file

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        
+        Args:
+            size: size of the square
+        """
+        self.__size = size
