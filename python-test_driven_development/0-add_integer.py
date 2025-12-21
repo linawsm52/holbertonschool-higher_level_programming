c0957bc3696fa33477223c7ee43d57bffb713000
linawsm52 <lenaexo0@gmail.com>
2025-12-21 07:17:30 +0000

    fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,4 +9,14 @@ def add_integer(a, b=98):
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    try:
+        a = int(a)
+    except (ValueError, OverflowError):
+        raise TypeError("a must be an integer")
+
+    try:
+        b = int(b)
+    except (ValueError, OverflowError):
+        raise TypeError("b must be an integer")
+
+    return a + b
