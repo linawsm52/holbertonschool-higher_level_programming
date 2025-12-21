a92c2e728ba6c1eefacd3a1099ee31cf6d315534
linawsm52 <lenaexo0@gmail.com>
2025-12-21 07:33:56 +0000

    fix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""2-matrix_divided module."""
+"""Divides all elements of a matrix."""
 
 
 def matrix_divided(matrix, div):
-    """Divide all elements of a matrix."""
+    """Return a new matrix with each element divided by div and rounded to 2 decimals."""
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
-            not all(row for row in matrix)):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
-
-    if not all(all(isinstance(x, (int, float)) for x in row) for row in matrix):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
+            not all(row for row in matrix) or
+            not all(isinstance(x, (int, float)) for row in matrix for x in row)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     row_len = len(matrix[0])
     if not all(len(row) == row_len for row in matrix):
@@ -22,7 +16,18 @@ def matrix_divided(matrix, div):
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return [[round(x / div, 2) for x in row] for row in matrix]
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for x in row:
+            value = round(x / div, 2)
+            if value == 0:
+                value = 0.0
+            new_row.append(value)
+        new_matrix.append(new_row)
+
+    return new_matrix
