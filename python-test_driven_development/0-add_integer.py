e3ccea95a022d85e30a6081cb5d497d3adfa9c40
linawsm52 <lenaexo0@gmail.com>
2025-12-21 07:08:12 +0000

    fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,15 +3,15 @@
 
 
 def add_integer(a, b=98):
-    """Return the sum of two integers."""
-    if type(a) not in [int, float]:
+    """Return the addition of a and b."""
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
-    if a != a or a in (float("inf"), float("-inf")):
+    if a != a or a == float("inf") or a == float("-inf"):
         raise TypeError("a must be an integer")
-    if b != b or b in (float("inf"), float("-inf")):
+    if b != b or b == float("inf") or b == float("-inf"):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
