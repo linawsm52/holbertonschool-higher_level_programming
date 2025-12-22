b959cb04f7650d88989b35d23abcacb13694ef3f
linawsm52 <lenaexo0@gmail.com>
2025-12-22 06:30:44 +0000

    new file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Module 0-read_file
+Reads a text file (UTF8) and prints it to stdout.
+"""
+
+
 def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
