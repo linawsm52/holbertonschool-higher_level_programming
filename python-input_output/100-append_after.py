8e189d23e72dfea4f35b110c0c45e181d18628db
linawsm52 <lenaexo0@gmail.com>
2025-12-22 06:58:39 +0000

    new file

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module 100-append_after
+Inserts a line of text to a file after each line containing a specific string.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts new_string after each line in the file that contains search_string.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    new_lines = []
+    for line in lines:
+        new_lines.append(line)
+        if search_string in line:
+            new_lines.append(new_string)
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(new_lines)
