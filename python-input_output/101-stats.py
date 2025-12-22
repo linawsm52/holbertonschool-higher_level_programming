30ecd0a36bf1cada1773e13e82f526d13445d68d
linawsm52 <lenaexo0@gmail.com>
2025-12-22 07:00:58 +0000

    new file

---
+++ python-input_output/101-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import sys
+
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+total_size = 0
+line_count = 0
+
+
+def print_stats():
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+try:
+    for line in sys.stdin:
+        parts = line.split()
+        try:
+            total_size += int(parts[-1])
+            if parts[-2] in status_codes:
+                status_codes[parts[-2]] += 1
+        except (IndexError, ValueError):
+            pass
+
+        line_count += 1
+        if line_count % 10 == 0:
+            print_stats()
+
+except KeyboardInterrupt:
+    print_stats()
+    raise
