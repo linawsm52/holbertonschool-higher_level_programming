920da71123acf9a48715eaee0475c4163a766f60
linawsm52 <lenaexo0@gmail.com>
2025-12-22 06:47:10 +0000

    new file

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 8-class_to_json
+Returns the dictionary description with simple data structure
+for JSON serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
+    """
+    return obj.__dict__
