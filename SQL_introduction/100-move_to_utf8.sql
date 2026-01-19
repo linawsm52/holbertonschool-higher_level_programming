3d3517cbd6508f36a1351c18d22d545c75818752
linawsm52 <lenaexo0@gmail.com>
2026-01-19 13:53:18 +0000

    new file

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -9,7 +9,13 @@ ALTER TABLE first_table
   CONVERT TO CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
 
+-- Force the column to utf8mb4 (safe) ...
 ALTER TABLE first_table
   MODIFY name VARCHAR(256)
-  CHARACTER SET DEFAULT
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+-- ... then remove explicit charset so SHOW CREATE TABLE matches the checker
+ALTER TABLE first_table
+  MODIFY name VARCHAR(256)
   COLLATE utf8mb4_unicode_ci;
