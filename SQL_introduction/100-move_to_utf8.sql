9ba8b7d0a646cf9d61a2e9872a03879e13878b35
linawsm52 <lenaexo0@gmail.com>
2026-01-19 14:08:07 +0000

    new file

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,8 @@
 -- Go to UTF8
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
 USE hbtn_0c_0;
 
 DROP TABLE IF EXISTS first_table;
@@ -8,3 +12,4 @@ CREATE TABLE first_table (
   name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   score INT DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
