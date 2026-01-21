3b49781a84e1ff7cf422a10c7aa1adbb91e96eee
linawsm52 <lenaexo0@gmail.com>
2026-01-21 15:59:44 +0000

    new file

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Cities table
+CREATE DATEBASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+	FOREIGN KEY (states_id) REFERENDES states(id)
+);
