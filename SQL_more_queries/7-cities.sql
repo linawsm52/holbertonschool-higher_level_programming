15d66866ec14cd9f80269a9b49e4d34973634c02
linawsm52 <lenaexo0@gmail.com>
2026-01-21 16:03:50 +0000

    new file

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,10 @@
 -- Cities table
-CREATE TABLE IF NOT EXISTS cities(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL
-	FOREIGN KEY (states_id) REFERENDES states(id)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
