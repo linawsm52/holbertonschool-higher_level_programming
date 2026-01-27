5de9169f0c91d09ef19c8873530f8581b2800fbc
linawsm52 <lenaexo0@gmail.com>
2026-01-27 15:07:22 +0000

    new file

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Number of shows by genre
+SELECT
+    tv_genres.name genre,
+    COUNT(tv_show_genres.show_id) number_of_shows
+FROM tv_genres
+JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
