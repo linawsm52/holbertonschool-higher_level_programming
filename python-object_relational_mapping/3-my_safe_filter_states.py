141ba013e3ca4bf6ee684706b1b587fd6cd19295
linawsm52 <lenaexo0@gmail.com>
2026-02-03 13:32:26 +0000

    new file

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Filter states by user input"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db_name
+    )
+
+    cur = db.cursor()
+    query = (
+        "SELECT * FROM states "
+        "WHERE BINARY name = %s "
+        "ORDER BY id ASC;"
+    )
+    cur.execute(query, (state_name,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
