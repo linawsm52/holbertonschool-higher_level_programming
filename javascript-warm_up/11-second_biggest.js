956ef168b03254bb5158e6c5173e71fd01180e0a
linawsm52 <lenaexo0@gmail.com>
2026-04-26 14:54:08 +0300

    new file

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-
-
-    const args = process.argv.slice(2).map(Number);
-
+  const args = process.argv.slice(2).map(Number);
   const uniqueSorted = [...new Set(args)].sort((a, b) => b - a);
-
   if (uniqueSorted.length > 1) {
     console.log(uniqueSorted[1]);
   } else {
